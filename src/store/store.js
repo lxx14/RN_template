67e7759f1063ae42c102257b0deaaf670683bb1c
AlexeyIlchenko <alexeyi@wizardsdev.com>
2019-11-26 14:39:46 +0200

    add simple instruction

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,10 @@ import rootReducer from '../redux/rootReducer';
 //import { composeWithDevTools } from 'remote-redux-devtools';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+// https://github.com/jhen0409/react-native-debugger/releases   ----download debugger
+// install and launch downloaded debugger
+// go to app, open command panel (cntrl+m) and debug JS remotely
+
 const middleware = [thunk];
 
 const store = createStore(rootReducer, compose(composeWithDevTools(applyMiddleware(...middleware))));
